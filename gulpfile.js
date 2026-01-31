ba477ab1fcc66a93d8f1337f9124bc0b43853f1b
FelipeReina12 <jfreinap@academia.usbbog.edu.co>
2026-01-30 22:31:30 -0500

    Actualizando código

--- gulpfile.js
+++ gulpfile.js
@@ -7,13 +7,13 @@ const sass = gulpSass(dartSass);
 
 export function css(done) {
     src("src/scss/app.scss")
-        .pipe(sass())
+        .pipe(sass().on("error", sass.logError))
         .pipe(dest("build/css"));
 
     done();   
 }
 
 export function dev() {
-    watch('src/scss/app.scss', css)
-
+    watch('src/scss/**/*.scss', css);  // los ** indican que puede haber carpetas dentro de carpetas 
+                                      // y buscar en todas ellas un archivo con extension .scss
 }
